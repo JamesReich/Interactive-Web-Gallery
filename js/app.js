7e16f2a500539c602dea2f058f6fc51b10d6d82d
James Reich <jamesgreich@gmail.com>
2022-04-09 16:03:54 -0700

    Final
    Should exceed expectations

--- js/app.js
+++ js/app.js
@@ -2,3 +2,18 @@
 
 baguetteBox.run('.gallery');
 
+
+function searchBar() {
+    let input = document.getElementById('search').value
+    input = input.toLowerCase();
+    let images = document.querySelectorAll('a');
+      
+    for (i = 0; i < images.length; i++) { 
+        if (!images[i].getAttribute('data-caption').toLowerCase().includes(input)) {
+            images[i].style.display="none";
+        }
+        else {
+            images[i].style.display="list-item";                 
+        }
+    }
+}
